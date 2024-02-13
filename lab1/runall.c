2ceb4b3406a74cc56449a11639149e6912202be5
BrendanBov <brendanbovenschen03@gmail.com>
2024-02-13 00:32:55 -0600

    Update runall.c

--- lab1/runall.c
+++ lab1/runall.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include "./src/shell.h"
 
 #define BUFSIZE 128
 
+int testsFailed = 0;
+
 int parse_output() {
-    char *cmd = "find ./testing -type f -name *.memfile";    
-    
-    char buf[BUFSIZE] = {0};
-    FILE *fp;
+    char* cmd = "find ./testing -type f -name *.memfile";
+
+    char buf[BUFSIZE] = { 0 };
+    FILE* fp;
 
     if ((fp = popen(cmd, "r")) == NULL) {
         printf("Error opening pipe!\n");
@@ -15,13 +19,17 @@ int parse_output() {
     }
 
     while (fgets(buf, BUFSIZE, fp) != NULL) {
-        // Do whatever you want here...
-        //printf("OUTPUT: %s", buf);
-        //system(strcat("./src/sim ",buf));
+        printf("%s\n", buf);
         char run[BUFSIZE] = "./src/sim ";
-        strcat(run,buf);
+        strcat(run, buf);
         system(run);
-        system("go");
+        //system("g\n");
+        //check for success or fail
+        //int testReg = CURRENT_STATE.REGS[10];
+        //if(testReg == 17)
+        //    testsFailed++;
+        //system("q\n");
+        //write(STDIN_FILENO, "\ng\n", BUFSIZE);
     }
 
     if (pclose(fp)) {
@@ -35,5 +43,6 @@ int parse_output() {
 int main()
 {
     parse_output();
+    printf("\n\nTests Failed : %d\n\n", testsFailed);
     return 0;
 }
